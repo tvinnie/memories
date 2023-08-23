ec62e7a8c0598729b9b8c1fb7230e4138b1e77ab
tvinnie <tvinnie77@gmail.com>
2023-08-23 17:01:33 +0300

    added authentication

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import {createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 import './index.css';
@@ -11,11 +11,10 @@ const store = createStore(reducers, compose(applyMiddleware(thunk)))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  // <React.StrictMode>
     <Provider store={store} >
       <App />
     </Provider>
-  // </React.StrictMode>
+    
 );
 
 
